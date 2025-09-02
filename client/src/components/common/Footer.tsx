e49baa408ac7c40eca6d90317604406eeac1cf09
aigle-levant <aiglelevant@gmail.com>
2025-09-02 11:20:34 +0530

    Added footer for desktop

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -16,37 +16,74 @@ export default function Footer() {
           </p>
         </div>
       </div>
-      <div id="footer-content" className="flex flex-row justify-around py-4">
+      <div id="footer-content" className="flex flex-row justify-around py-6">
         <div id="title-socials-container">
           <div id="title-container" className="flex flex-col">
-            <p className="font-heading font-bold">Doripomo</p>
+            <p className="font-heading font-bold mb-4 text-4xl">Doripomo</p>
           </div>
-          <div id="socials-container" className="flex flex-col font-body">
+          <div id="socials-container" className="flex flex-col font-body gap-2">
             <NavLink to="https://www.linkedin.com/in/prajanya-subramanian">
-              LinkedIn
+              <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                LinkedIn
+              </p>
             </NavLink>
             <NavLink to="https://github.com/aigle-levant/doripomo">
-              GitHub
+              <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                GitHub
+              </p>
             </NavLink>
             <NavLink to="https://x.com/aiglelevant">
-              X [formerly known as Twitter]
+              <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                X [also known as Twitter]
+              </p>
             </NavLink>
           </div>
         </div>
-        <div id="get-started-links-container" className="flex flex-col">
-          <p className="font-heading font-bold">Get started</p>
-          <div id="get-started-links" className="font-body flex flex-col">
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/signup">Sign up</NavLink>
+        <div
+          id="links-container"
+          className="flex flex-row justify-around gap-14"
+        >
+          <div id="get-started-links-container" className="flex flex-col">
+            <p className="font-heading font-bold mb-2 text-2xl">Get started</p>
+            <div id="get-started-links" className="font-body flex flex-col">
+              <NavLink to="/login">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  Login
+                </p>
+              </NavLink>
+              <NavLink to="/signup">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  Sign up
+                </p>
+              </NavLink>
+            </div>
           </div>
-        </div>
-        <div id="features-links-container" className="flex flex-col">
-          <p className="font-heading font-bold">Features we offer</p>
           <div id="features-links-container" className="flex flex-col">
-            <NavLink to="/pomodoro">Pomodoro</NavLink>
-            <NavLink to="/syllabus">Syllabus</NavLink>
-            <NavLink to="/about">About us</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
+            <p className="font-heading font-bold mb-2 text-2xl">
+              Features we offer
+            </p>
+            <div id="features-links-container" className="flex flex-col">
+              <NavLink to="/pomodoro">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  Pomodoro
+                </p>
+              </NavLink>
+              <NavLink to="/syllabus">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  Syllabus
+                </p>
+              </NavLink>
+              <NavLink to="/about">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  About us
+                </p>
+              </NavLink>
+              <NavLink to="/blog">
+                <p className="text-gray-700 hover:font-bold hover:text-night dark:text-gray-500 hover:dark:text-pale">
+                  Blog
+                </p>
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
