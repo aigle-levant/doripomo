e086db3ed7ba87a150276d127c442c27db6749bb
aigle-levant <aiglelevant@gmail.com>
2025-09-05 17:24:22 +0530

    Worked with server

--- server/config/app.ts
+++ server/config/app.ts
@@ -1,11 +1,8 @@
 import { limiter } from "./../utils/rateLimit.js";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import { authRouter } from "../routes/auth.routes.js";
 import helmet from "helmet";
-
-dotenv.config({ path: "../.env" });
 
 // env variables
 export const port = process.env.PORT || 4000;
--- server/config/mongo.ts
+++ server/config/mongo.ts
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 // get env variables
@@ -8,6 +6,7 @@ const password = process.env.MONGO_PASSWORD;
 if (!username || !password) {
   throw new Error("MONGO_USER or MONGO_PASSWORD is missing in .env");
 }
+
 export const connectionString = `mongodb+srv://${username}:${password}@doripomo.nfdu5jb.mongodb.net/?retryWrites=true&w=majority&appName=doripomo`;
 
 export const client = new MongoClient(connectionString, {
